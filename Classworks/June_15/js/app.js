7e77f699015bea24a6dbd00de996c6c1aa3e6c9e
Grigol Tchkoidze <grigol.tchkoidze@gmail.com>
2018-06-16 11:14:41 +0400

    added june 16 classwork

---
+++ Classworks/June_15/js/app.js
@@ -0,0 +1,35 @@
+"use strict";
+// let num = prompt("Enter a number");
+// ==, ===, !=, !==, >, <, >=, <=
+// &&, ||, !
+
+// if (num % 2 == 0) {
+//     alert("Number is even");
+// } else {
+//     alert("Number is odd");
+// }
+
+
+
+// let x;
+// if (x = 5) {
+//     alert("false");
+//     x = 6;
+// }
+
+
+
+// let accessAllowed;
+// let age = prompt("age");
+// if (age > 18) {
+//     accessAllowed = true;
+// } else {
+//     accessAllowed = false;
+// }
+
+// accessAllowed = (age > 18) ? true : false;
+
+// accessAllowed = age > 18;
+
+// alert(accessAllowed);
+// let result = condition ? value_1 : value_2;
