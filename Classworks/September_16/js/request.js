6a5e8e0f32cedd1a91c0f87cc439e2a6055c3b50
Grigol Tchkoidze <grigol.tchkoidze@gmail.com>
2018-09-16 16:16:44 +0400

    added september 16 classwork

---
+++ Classworks/September_16/js/request.js
@@ -0,0 +1,15 @@
+/* 
+    Request module
+*/
+
+
+const request = require('request');
+
+request('http://techub.ge', (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        console.log(body);
+    }
+    else {
+        console.log(error);
+    }
+});
